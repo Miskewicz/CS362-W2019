2f274152218ea8ffd4803547c0c282ddbd0d6f25
Miskewicz <miskewie@oregonstate.edu>
2019-02-08 17:24:51 -0500

    Update unittest3.c
    Additional print messages

--- projects/miskewie/dominion/unittest3.c
+++ projects/miskewie/dominion/unittest3.c
@@ -22,6 +22,8 @@ int main(){
     initializeGame(2, kCards, 1, &gs);
     gsOrig = gs;
 
+    printf("\nTest 1: Coins in deck/discard - none in hand\n");
+    printf("========================================\n");
     //coins in deck+discard, none in hand - should be 0 coins
     gs.deckCount[0] = 2;
     gs.deck[0][0] = copper;
@@ -32,11 +34,13 @@ int main(){
     gs.discardCount[0] = 1;
     gs.discard[0][0] = gold;
     gsCopy = gs;
-    updateCoins(0, &gs, 0);
+    checkValue(updateCoins(0, &gs, 0), 0, "function returns success", 0);
     checkValue(gs.coins, 0, "coins in deck+discard, none in hand", 1);
     gsCopy.coins = gs.coins;
     compareGameState(&gs, &gsCopy); //should be no state change
 
+    printf("\nTest 2: Non-coin cards in hand\n");
+    printf("========================================\n");
     //one of every non coin card in hand - should be 0 coins
     gs = gsOrig;
     gs.handCount[0] = 0;
@@ -47,20 +51,24 @@ int main(){
         }
     }
     gsCopy = gs;
-    updateCoins(0, &gs, 0);
+    checkValue(updateCoins(0, &gs, 0), 0, "function returns success", 0);
     checkValue(gs.coins, 0, "every non coin card in hand", 1);
     gsCopy.coins = gs.coins;
     compareGameState(&gs, &gsCopy); //should be no state change
 
+    printf("\nTest 3: Empty hand\n");
+    printf("========================================\n");
     //completely empty hand - should be 0 coins
     gs = gsOrig;
     gs.handCount[0] = 0;
     gsCopy = gs;
-    updateCoins(0, &gs, 0);
+    checkValue(updateCoins(0, &gs, 0), 0, "function returns success", 0);
     checkValue(gs.coins, 0, "empty hand", 1);
     gsCopy.coins = gs.coins;
     compareGameState(&gs, &gsCopy); //should be no state change
 
+    printf("\nTest 4: Coins in hand\n");
+    printf("========================================\n");
     //1 coin in hand of each type - should be value of the respective coin
     coinValue = 1;
     for(i = copper; i <=gold; i++){
@@ -68,13 +76,15 @@ int main(){
         gs.hand[0][0] = i;
         gs.handCount[0] = 1;
         gsCopy = gs;
-        updateCoins(0, &gs, 0);
+        checkValue(updateCoins(0, &gs, 0), 0, "function returns success", 0);
         sprintf(buffer, "single coin in hand id: %i", i);
         checkValue(gs.coins, coinValue++, buffer, 1);
         gsCopy.coins = gs.coins;
         compareGameState(&gs, &gsCopy); //should be no state change
     }
 
+    printf("\nTest 5: Coins in hand - each player\n");
+    printf("========================================\n");
     //1 of each coin in hand - each player
     for(player = 0; player < MAX_PLAYERS; player++){
         gs = gsOrig;
@@ -84,13 +94,15 @@ int main(){
             gs.hand[player][j++] = i;
         }
         gsCopy = gs;
-        updateCoins(player, &gs, 0);
+        checkValue(updateCoins(player, &gs, 0), 0, "function returns success", 0);
         sprintf(buffer, "1 of each coin player %i", player);
         checkValue(gs.coins, 6, buffer, 1);
         gsCopy.coins = gs.coins;
         compareGameState(&gs, &gsCopy); //should be no state change
     }
 
+    printf("\nTest 6: MAX_HAND full of copper\n");
+    printf("========================================\n");
     //MAX_HAND full of copper
     gs = gsOrig;
     gs.handCount[0] = MAX_HAND;
@@ -98,7 +110,7 @@ int main(){
         gs.hand[0][i] = copper;
     }
     gsCopy = gs;
-    updateCoins(0, &gs, 0);
+    checkValue(updateCoins(0, &gs, 0), 0, "function returns success", 0);
     checkValue(gs.coins, MAX_HAND, "hand full of copper", 1);
     gsCopy.coins = gs.coins;
     compareGameState(&gs, &gsCopy); //should be no state change
