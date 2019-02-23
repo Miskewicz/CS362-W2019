3cafddaeef4c8e33909703534e52a08f3b6a64ed
Miskewicz <miskewie@oregonstate.edu>
2019-02-23 13:58:35 -0500

    Create random_test_helpers.c
    added functions to randomize gameState

---
+++ projects/miskewie/dominion/random_test_helpers.c
@@ -0,0 +1,103 @@
+#include "dominion.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int randBetween(int min, int max){
+    return(rand() % (max - min + 1) + min);
+}
+
+int getRandomCard(){
+    return(randBetween(curse, treasure_map));
+}
+
+void randomizeSupply(struct gameState* gs){
+    int i;
+    for(i = curse; i <= treasure_map; i++){
+        gs->supplyCount[i] = randBetween(0, MAX_DECK);
+    }
+}
+
+void randomizePlayedCards(struct gameState* gs){
+    int i;
+
+    //played card count
+    gs->playedCardCount = randBetween(0, MAX_HAND);
+
+    //populate played cards
+    for(i = 0; i < gs->playedCardCount; i++){
+        gs->playedCards[i] = getRandomCard();
+    }
+}
+
+void randomizeDiscard(struct gameState* gs, int player){
+    int i;
+
+    //discard size
+    gs->discardCount[player] = randBetween(0, MAX_DECK);
+
+    //populate discard pile
+    for(i = 0; i < gs->discardCount[player]; i++){
+        gs->discard[player][i] = getRandomCard();
+    }
+}
+
+void randomizeHand(struct gameState* gs, int player){
+    int i;
+
+    //hand size
+    gs->handCount[player] = randBetween(0, MAX_HAND);
+
+    //populate hand
+    for(i = 0; i < gs->handCount[player]; i++){
+        gs->hand[player][i] = getRandomCard();
+    }
+}
+
+void randomizeDeck(struct gameState* gs, int player){
+    int i;
+    
+    //deck size
+    gs->deckCount[player] = randBetween(0, MAX_DECK);
+
+    //populate deck
+    for(i = 0; i < gs->deckCount[player]; i++){
+        gs->deck[player][i] = getRandomCard();
+    }
+}
+
+
+//generate a completely random gameState
+void randomizeGameState(struct gameState* gs){
+
+    int i;
+
+    //zero gameState
+    memset(gs, 0, sizeof(struct gameState));
+
+    //random players
+    gs->numPlayers = randBetween(2, MAX_PLAYERS);
+
+    //for each player
+    for(i = 0; i < gs->numPlayers; i++){
+        //random deck
+        randomizeDeck(gs, i);
+
+        //random hand
+        randomizeHand(gs, i);
+
+        //random discard
+        randomizeDiscard(gs, i);
+    }
+
+    //played cards
+    randomizePlayedCards(gs);
+
+    //supply
+    randomizeSupply(gs);
+
+    //randomize other gameState values
+    gs->numActions = randBetween(0, 25);
+    gs->numBuys = randBetween(0, 25);
+    gs->coins = randBetween(0, 100);
+}
